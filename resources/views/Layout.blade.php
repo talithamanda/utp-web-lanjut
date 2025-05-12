06ee5aabf4ddeebc3e6cc9a2f69f8368dcc87149
talithamanda <talithaamanda49@gmail.com>
2025-05-12 18:21:34 +0700

    Part 6

--- resources/views/Layout.blade.php
+++ resources/views/Layout.blade.php
@@ -82,7 +82,7 @@ div.content {
                         <a class="active" href="#home">Home</a>
                         <a href="{{ url('/students') }}">Student</a>
                         <a href="{{ url('/teachers') }}">Teacher</a>
-                        <a href="#contact">Courses</a>
+                        <a href="{{ url('/courses') }}">Courses</a>
                         <a href="#about">Enrollment</a>
                         <a href="#contact">Payment</a>
                     </div>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\TeacherController;
+use App\Http\Controllers\CourseController;
+use App\Models\Course;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,5 +21,5 @@ Route::get('/', function () {
     return view('Layout');
 });
 Route::resource('/students', StudentController::class);
-
 Route::resource('/teachers', TeacherController::class);
+Route::resource('/courses', CourseController::class);
